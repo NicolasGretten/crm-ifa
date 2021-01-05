8cffe5640c3aa8ff4c9e630023c58819d4345f47
nico <ngretten@gmail.com>
2021-01-05 15:56:07 +0100

    customer entity

---
+++ migrations/Version20210105145514.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210105145514 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE customer (id INT AUTO_INCREMENT NOT NULL, company_id_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, first_name VARCHAR(255) NOT NULL, email_address VARCHAR(255) NOT NULL, access TINYINT(1) NOT NULL, INDEX IDX_81398E0938B53C32 (company_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE customer ADD CONSTRAINT FK_81398E0938B53C32 FOREIGN KEY (company_id_id) REFERENCES company (id)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE customer');
+    }
+}
---
+++ src/Entity/Customer.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CustomerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CustomerRepository::class)
+ */
+class Customer
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Company::class)
+     */
+    private $company_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email_address;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $access;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCompanyId(): ?Company
+    {
+        return $this->company_id;
+    }
+
+    public function setCompanyId(?Company $company_id): self
+    {
+        $this->company_id = $company_id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getEmailAddress(): ?string
+    {
+        return $this->email_address;
+    }
+
+    public function setEmailAddress(string $email_address): self
+    {
+        $this->email_address = $email_address;
+
+        return $this;
+    }
+
+    public function getAccess(): ?bool
+    {
+        return $this->access;
+    }
+
+    public function setAccess(bool $access): self
+    {
+        $this->access = $access;
+
+        return $this;
+    }
+}
